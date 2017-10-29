036f4a22ce6f962761997ee19eb4d6eeaeb560ec
diogoaromao <diogoaromao@gmail.com>
2017-10-29 11:04:37 +0000

    Movie

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        List<Movie> movies = new List<Movie>
+        {
+            new Movie
+            {
+                Id = 1,
+                Name = "Shrek!"
+            },
+            new Movie
+            {
+                Id = 2,
+                Name = "Finding Nemo!"
+            }
+        };
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(movies);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var movie = movies.Where(m => m.Id == id).FirstOrDefault();
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+    }
+}
